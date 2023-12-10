064e71939215930797c2284946ae76ae6f2b0491
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-12-10 13:46:49 +0530

    added a footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { NavBar } from "@/components";
+import { Footer, NavBar } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar  />
-        {children}</body>
+        <NavBar />
+        {children}
+      </body>
+      <Footer />
     </html>
   );
 }
