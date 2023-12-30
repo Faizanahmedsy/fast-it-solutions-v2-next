ec6e418eb240bbe3b4a0796f6058ea52bb4576b7
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-12-30 19:23:57 +0530

    adddd servivves pages

---
+++ src/lib/constants/servicesConstants.js
@@ -0,0 +1,33 @@
+export const SERVICES = [
+  {
+    title: "Program/Project Manager",
+    description:
+      "A program/project manager defines what success looks like for a program and project and gathers a team to make that vision a reality. A program/project manager finds the customer need and the bigger company objectives that a project or feature will satisfy.",
+  },
+  {
+    title: "Release Training Engineer",
+    description:
+      "A product manager(RTE) is a person who identifies the customer need and the larger business objectives that a product or feature will fulfill articulates what success looks like for a product, and rallies a team to turn that vision into a reality.",
+  },
+  {
+    title: "Scrum Master",
+    description:
+      "A facilitator for an Agile development team is a scrum master. They are in charge of overseeing information sharing among team members. A team can communicate and self-organize using the Scrum project management framework to make changes fast, in accordance with Agile principles.",
+  },
+  {
+    title: "Security Analyst",
+    description:
+      "A facilitator for an Agile development team is a scrum master. They are in charge of overseeing information sharing among team members. A team can communicate and self-organize using the Scrum project management framework to make changes fast, in accordance with Agile principles.",
+  },
+  {
+    title: "Security Auditor",
+    description:
+      "Security auditors create and execute audits based on organizational policies and governmental regulations. To inspect and assess security controls and practices, security auditors work closely with IT professionals, managers, and executives.",
+  },
+  {
+    title: "Vulnerability and Risk Analyst",
+    description: "Say something interesting about your business here",
+  },
+];
+
+export const colors = ["#5E25D9", "2E1A73", "F25D27", "F2A88D", "F2F2F2"];
